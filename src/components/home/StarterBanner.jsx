6c57fdf26254e181546ee416e03001d6caef345e
Asif A Owadud <asifaowadud@gmail.com>
2024-06-09 05:02:46 +0600

    starter banner done

--- src/components/home/StarterBanner.jsx
+++ src/components/home/StarterBanner.jsx
@@ -3,26 +3,33 @@ import { useLocation } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { Typewriter } from "react-simple-typewriter";
 import bannerImg from "./../../assets/images/banner.gif";
+import bannerImg2 from "./../../assets/images/cover.png";
 
 const StarterBanner = () => {
   const location = useLocation();
   if (location.pathname.includes("allBlogs")) return null;
   return (
-    <div id="openingBanner">
-      <div className="hero min-h-screen bg-[#B1C0CB]">
-        <div className="hero-content flex-col lg:flex-row-reverse text-white">
-          <img
-            src={bannerImg}
-            alt=""
-            className="md:h-[600px] md:w-1/2 object-cover hidden lg:block"
-          />
-          <div className="text-center lg:text-left">
-            <h1 className="text-4xl sm:text-6xl font-bold">
-              Hello! I'M <span className="text-black">ASIF</span>{" "}
+    <>
+      <div id="openingBanner">
+        <div
+          className="relative flex flex-col items-center justify-center text-center text-white bg-cover bg-center h-[100vmin]"
+          style={{ backgroundImage: `url(${bannerImg2})` }}
+        >
+          <div className="banner-after"></div>
+          <p className="font-light text-[1.4vmax] font-[Lucida Sans]">Hello!</p>
+          <div className="my-[2vmax]">
+            <h1 className="font-semibold text-[2.5vmax] uppercase font-[Roboto]">
+              I'M{" "}
+              <span className="border rounded-lg px-2  text-white ms-3">
+                ASIF A OWADUD
+              </span>{" "}
             </h1>
-            <h2 className="text-3xl py-6">
-              I am a <br className="block sm:hidden" />
-              <span style={{ color: "black", fontWeight: "bold" }}>
+
+            <h2 className="text-2xl py-5">
+              I am a <br className="block sm:hidden " />
+              <span
+                className="border rounded-lg px-3 bg-white text-black ms-2 font-bold" /* style={{ color: "black", fontWeight: "bold" }} */
+              >
                 <Typewriter
                   words={[
                     " Frontend Developer",
@@ -35,18 +42,19 @@ const StarterBanner = () => {
                 _
               </span>
             </h2>
-            <HashLink smooth to="#banner">
-              <button className="btn btn-wide btn-outline text-xl rounded-none rounded-bl-3xl rounded-tr-3xl hover:rounded-none transition-all">
-                Let's start
-                <span className="ml-4">
-                  <FaArrowCircleDown />
-                </span>
-              </button>
-            </HashLink>
           </div>
+
+          <HashLink smooth to="#banner">
+            <button className="btn btn-wide outline outline-1 hover:outline-white text-xl rounded-none rounded-bl-3xl rounded-tr-3xl hover:rounded-none hover:bg-transparent hover:text-white transition-all text-black bg-white">
+              Let's start
+              <span className="ml-4">
+                <FaArrowCircleDown />
+              </span>
+            </button>
+          </HashLink>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
